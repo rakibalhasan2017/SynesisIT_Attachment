51182707f8eba7b34d740364182d964e51ac4287
rakib <rakuhasan2017@gmai.com>
2025-11-10 04:52:51 +0600

    llm is ready to use

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+import os
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv, find_dotenv
+
+
+
+load_dotenv(find_dotenv())
+HF_TOKEN = os.environ.get("HF_TOKEN")
+HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
+
+
+def load_llm(huggingface_repo_id):
+    llm = HuggingFaceEndpoint(
+        repo_id=huggingface_repo_id,
+        temperature=0.5,
+        model_kwargs={"token": HF_TOKEN, "max_length": "512"}
+    )
+    return llm
+
+
